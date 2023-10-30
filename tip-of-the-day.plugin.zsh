4f71e05c705bcbc57031ab8fc2e76e62ee57313a
Mubashshir <ahmubashshir@gmail.com>
2023-10-30 22:51:05 +0600

    Implement allowlist/denylist
    Signed-off-by: Mubashshir <ahmubashshir@gmail.com>

--- tip-of-the-day.plugin.zsh
+++ tip-of-the-day.plugin.zsh
@@ -14,18 +14,21 @@ then
 	builtin zcompile -Mz "${__zsh_tips_file}"
 fi
 
+# zsh_load_tips=(all -<module1 [-<module2> [...]]>) for denylist
+# zsh_load_tips=(<module1 [module2 [...]]>) for allowlist
 (( ${+zsh_load_tips} )) || typeset -ga zsh_load_tips
 (( ${+__zsh_loaded_tips} )) || typeset -ga __zsh_loaded_tips
 
 RANDOM=$(od -vAn -N4 -tu < /dev/urandom)
+
 tip-of-the-day()
 {
-	[[ -t 1 ]] || return
+	[[ -t 1 ]] || return 0
 	if [[ ${#__zsh_loaded_tips[@]} -eq 0 || $1 == --reload ]]; then
 		local is_tips_enabled_for="::$RANDOM/is_tips_enabled_for/$$"
 		local -a tips=()
 		function "$is_tips_enabled_for" {
-			[[ $#zsh_load_tips -eq 0 || ${zsh_load_tips[(r)"$1"]} == "$1" ]]
+			[[ $#zsh_load_tips -eq 0 || ("${zsh_load_tips[(r)-$1]}" != "-$1" && "${zsh_load_tips[(r)all]}" == "all") || "${zsh_load_tips[(r)$1]}" == "$1" ]]
 		}
 
 		builtin source "$__zsh_tips_file" && __zsh_loaded_tips=("${tips[@]}")
