ed3d8e3cc2c990be444d6c9c6ee664637acd95cc
Mubashshir <ahmubashshir@gmail.com>
2023-10-30 21:56:43 +0600

    Add new tips
    Signed-off-by: Mubashshir <ahmubashshir@gmail.com>

--- tips.zsh
+++ tips.zsh
@@ -20,3 +20,18 @@ $'You can Test a systemd calendar event multiple time using
 \x20
 \n> `systemd-analyze calendar "*-*-1/16 00:00:00" --iterations=4`'
 )
+
+[[ $#zsh_load_tips -eq 0 || ${zsh_load_tips[(r)shell]} == shell ]] && __zsh_loaded_tips+=(
+# cd-oldpwd
+$'You can go back to last working dir using `cd -`'
+# output-pause-unpause
+$'You can pause terminal output using Ctrl+S
+and unpause using Ctrl+Q'
+# follow-log-inode
+$'You can follow log in realtime using
+\t`tail -f <path>`'
+# follow-log-filename
+$'You can follow log path in realtime using
+\t`tail -F <path>`
+this continue to work even after log rotation'
+)
